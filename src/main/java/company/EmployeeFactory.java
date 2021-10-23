0c90d47ec5aa6b2adc6fc8d49d4f0ecc2451ea08
yehor96 <yehor.shyriaiev@gmail.com>
2021-10-23 15:36:00 +0300

    Convert project into maven

--- src/main/java/company/EmployeeFactory.java
+++ src/main/java/company/EmployeeFactory.java
@@ -7,9 +7,6 @@ import company.employees.Manager;
 import company.enums.EmployeeType;
 import company.enums.Gender;
 
-import static company.Randomizer.getBoolean;
-import static company.Randomizer.getIntWithin;
-
 public class EmployeeFactory {
 
     private final String[] maleNames = {"John", "Martin", "Michael", "Adam", "Colin", "Steve", "Marcus", "Ivan"};
@@ -25,24 +22,24 @@ public class EmployeeFactory {
     }
 
     public Employee generateEmployee() {
-        Gender gender = getGender(getBoolean());
+        Gender gender = getGender(Randomizer.getBoolean());
         String name = getName(gender);
-        int age = getIntWithin(18, 75);
+        int age = Randomizer.getIntWithin(18, 75);
 
         int salary;
         EmployeeType type = getRandomEmployeeType();
         switch (type) {
             case DEVELOPER:
-                salary = getIntWithin(10, 100) * 100;
-                int fixedBugs = getIntWithin(1, 10);
+                salary = Randomizer.getIntWithin(10, 100) * 100;
+                int fixedBugs = Randomizer.getIntWithin(1, 10);
                 return new Developer(name, age, salary, gender, fixedBugs);
             case MANAGER:
-                salary = getIntWithin(50, 200) * 100;
+                salary = Randomizer.getIntWithin(50, 200) * 100;
                 return new Manager(name, age, salary, gender);
             case DESIGNER:
-                salary = getIntWithin(5, 60) * 100;
-                double rate = getIntWithin(50, 150) * 1.5;
-                int workedDays = getIntWithin(1, 20);
+                salary = Randomizer.getIntWithin(5, 60) * 100;
+                double rate = Randomizer.getIntWithin(50, 150) * 1.5;
+                int workedDays = Randomizer.getIntWithin(1, 20);
                 return new Designer(name, age, salary, gender, rate, workedDays);
             default:
                 throw new IllegalStateException("Unexpected value: " + type.name());
@@ -51,14 +48,14 @@ public class EmployeeFactory {
 
     private EmployeeType getRandomEmployeeType() {
         EmployeeType[] types = EmployeeType.values();
-        return types[getIntWithin(0, types.length - 1)];
+        return types[Randomizer.getIntWithin(0, types.length - 1)];
     }
 
     private String getName(Gender gender) {
         if (gender == Gender.MALE) {
-            return maleNames[getIntWithin(0, maleNames.length - 1)];
+            return maleNames[Randomizer.getIntWithin(0, maleNames.length - 1)];
         } else {
-            return femaleNames[getIntWithin(0, femaleNames.length - 1)];
+            return femaleNames[Randomizer.getIntWithin(0, femaleNames.length - 1)];
         }
     }
 
